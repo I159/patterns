f73e6d4eeaddeec311b136f37d5262b2f7264b1c
Pekelny Ilya <pekelny@gmail.com>
2012-03-08 10:13:28 +0200

    decorators

---
+++ decorators/decorators.py
@@ -0,0 +1,39 @@
+# /usr/bin/env python
+# -*- coding:utf8 -*-
+
+from time import time
+
+
+def timer(f):
+    def wrap(*args, **kwargs):
+        t = time()
+        result = f(*args, **kwargs)
+        print "Run-time function: %f" % (time()-t)
+        return result
+    return wrap
+
+
+# As class
+class Timer(object):
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, *args, **kwargs):
+        t = time()
+        result = self.f(*args, **kwargs)
+        print "Run-time function: %f" % (time()-t)
+        return result
+
+
+# With arguments
+def replacer(p=None, a=None):
+    def _mid(f):
+        class Wrap(object):
+            def __init__(self, f, p, a):
+                self.f = f
+                self.p = p
+                self.a = a
+            def __call__(self, *args, **kwargs):
+                return self.f().replace(p, a)
+        return Wrap(f, p, a)
+    return _mid
---
+++ decorators/decorators_app.py
@@ -0,0 +1,30 @@
+# /usr/bin/env python
+# -*- coding:utf8 -*-
+
+from decorators import *
+
+@replacer(p='cute', a='insane')
+def squirrels():
+    return '1000 cute squirrels on my yard'
+
+
+@timer
+def multiplication(intg):
+    res = 0
+    for i in range(intg):
+        res = res + (intg * i)
+    return res
+
+
+@Timer
+def composition(intg):
+    res = 0
+    for i in range(intg):
+        res = res + (intg + i)
+    return res
+
+
+
+squirrels()
+multiplication(3)
+composition(3)
