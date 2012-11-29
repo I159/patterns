58e94b2009e737190a9f301f7e4232e81d80f7cb
I159 <pekelny@gmail.com>
2012-11-29 21:47:42 +0200

    The right way to build class decorator

--- singleton/singleton.py
+++ singleton/singleton.py
@@ -2,6 +2,9 @@
 # -*-coding:utf8-*-
 
 class Singleton(type):
+    """ Meta class singleton pattern.
+    should be used as ::__metaclass__ = Singleton
+    """
     def __init__(self, name, bases, dict):
         super(Singleton, self).__init__(name, bases, dict)
         self._instance = None
@@ -10,3 +13,40 @@ class Singleton(type):
         if self._instance is None:
             self._instance = super(Singleton, self).__call__()
         return self._instance
+
+
+def singleton(cls):
+    inst = {}
+    def get(*args, **kwargs):
+        cls_id = id(cls)
+        if cls_id not in inst:
+            inst[cls_id] = cls(*args, **kwargs)
+        return inst[cls_id]
+    return get
+
+
+class SingletonDecorator(object):
+    inst = {}
+    def __init__(self, cls):
+        self.cls = cls
+
+    def __call__(self, *args, **kwargs):
+        cls_id = id(self.cls)
+        if cls_id not in self.inst:
+            self.inst[cls_id] = self.cls(*args, **kwargs)
+        return self.inst[cls_id]
+
+
+class SD(object):
+    """ The right way to decorate classes """
+    inst = {}
+    def __init__(self, cls):
+        self.__dict__.update(cls.__dict__)
+        self.__name__ = cls.__name__
+        self.cls = cls
+
+    def __call__(self, *args, **kwargs):
+        if self.cls not in self.inst:
+            self.inst[self.cls] = self.cls(*args, **kwargs)
+        return self.inst[self.cls]
+
--- singleton/singleton_app.py
+++ singleton/singleton_app.py
@@ -1,16 +1,75 @@
 # !/usr/env/python python
 # -*-coding:utf8-*-
 
-from singleton import Singleton
+from singleton import Singleton, singleton, SD
 
 
 class NewClass(object):
     __metaclass__ = Singleton
 
+    def __init__(self):
+        print 'Created by Singleton __metaclass__ self', self
+
+    @classmethod
+    def cls_meth(cls):
+        print 'Created by Singleton __metaclass__ cls', cls
 
 
-a = NewClass()
-a.attr = 12
-b = NewClass()
-print b.attr
-print a is b
+# You can't inherit from this class, because it is not even class befor You
+# call it.
+@singleton
+class AnotherNewClass(object):
+    def __init__(self):
+        print 'Decorated by singleton decorator self', self
+
+    @classmethod
+    def cls_meth(cls):
+        print 'Decorated by singleton decorator  cls', cls
+
+
+
+@SD
+class OneMoreClass(object):
+    def __init__(self):
+        print self
+
+    @classmethod
+    def cls_meth(cls):
+        print cls.__name__
+
+
+def test_one_more_class():
+    print 'call', OneMoreClass()
+    print 'name: ', OneMoreClass.__name__
+    print 'classmethod', OneMoreClass.cls_meth
+
+
+
+def instance_creation():
+    print '------------------ Metaclass instance comparison -----------------'
+    a = NewClass()
+    a.attr = 12
+    b = NewClass()
+    print 'a.attr:', a.attr, 'b.attr', b.attr
+    print 'a is b', a is b
+
+    print '------------------ Decorated instance comparison -----------------'
+    a = AnotherNewClass()
+    a.attr = 12
+    b = AnotherNewClass()
+    print 'a.attr:', a.attr, 'b.attr', b.attr
+    print 'a is b', a is b
+
+def check_types():
+    print '---------------------- type of NewClass --------------------------'
+    print type(NewClass)
+    print '------------------ type of AnotherNewClass -----------------------'
+    print type(AnotherNewClass)
+    print '------------------ type of NewClass instance ---------------------'
+    print type(NewClass())
+    print '--------------- type of AnotherNewClass instance -----------------'
+    print type(AnotherNewClass())
+    print '----------------------- NewClass classmethod ---------------------'
+    print NewClass.cls_meth()
+    print '-------------------- AnotherNewClass classmethod -----------------'
+    print AnotherNewClass.cls_meth()
